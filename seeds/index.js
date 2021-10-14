3eafa512f41d6e23c332da0caddef08dfc8708d8
Aniket Pathak <aniketpathak028@gmail.com>
2021-10-14 13:42:58 +0530

    add image to campground model & beautify show page

--- seeds/index.js
+++ seeds/index.js
@@ -17,11 +17,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
-        const random1000 = Math.floor(Math.random() * 1000)
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-
+            title: `${sample(descriptors)} ${sample(places)}`,
+            img: 'https://source.unsplash.com/collection/9046579',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati consequatur optio sequi. Quam obcaecati libero alias dignissimos, eius ipsa maxime!',
+            price
         });
         await camp.save();
     }
