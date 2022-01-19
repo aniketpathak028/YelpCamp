10e8876b7e0548bda806ceb2130ce7142574406b
Aniket Pathak <aniketpathak028@gmail.com>
2022-01-19 16:55:43 +0530

    refactoring mongoose-middleware

--- models/campground.js
+++ models/campground.js
@@ -1,29 +1,31 @@
-const mongoose = require('mongoose');
-const Review = require('./review');
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title: String,
-    price: Number,
-    img: String,
-    description: String,
-    location: String,
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+  title: String,
+  price: Number,
+  img: String,
+  description: String,
+  location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+CampgroundSchema.post("findOneAndDelete", async function (doc, next) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  } else {
+    next();
+  }
+});
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
